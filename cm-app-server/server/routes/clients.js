a6243dc7e9838b9333486538f8b384a563f5e062
mleitao27 <mleitao27@gmail.com>
2022-03-29 08:37:51 +0100

    check if client fiscal number exists already

--- cm-app-server/server/routes/clients.js
+++ cm-app-server/server/routes/clients.js
@@ -19,28 +19,32 @@ router.get('/:id', async (req, res) => {
 
 // Add clients
 router.post('/', async (req, res) => {
-    let clientId = await db.insertDocument('clients', {
-        name: req.body.name,
-        address: req.body.address,
-        fiscalNumber: req.body.fiscalNumber,
-        createdAt: new Date()
-    })
-    // If no beneficiaries were added
-    // or were added and then removed
-    // add the client as beneficiary
-    if(req.body.beneficiaries && req.body.beneficiaries.length) {
-        req.body.beneficiaries.map(async(b) => {
-            await db.insertDocument('beneficiaries', {
-                clientId: clientId.insertedId,
-                name: b.name,
-                address: b.address,
-                fiscalNumber: b.fiscalNumber,
-                isClient: b.isClient,
-                createdAt: new Date()
-            })
+    // Check if client already exists
+    let alreadyClient = await db.getDocument('clients', {fiscalNumber: req.body.fiscalNumber});
+    if(alreadyClient.length) res.status(409).send('already_exist_client_fiscal_number');
+    else {
+        let clientId = await db.insertDocument('clients', {
+            name: req.body.name,
+            address: req.body.address,
+            fiscalNumber: req.body.fiscalNumber,
+            createdAt: new Date()
         })
-    } else {
-        await db.insertDocument('beneficiaries', {
+        // If no beneficiaries were added
+        // or were added and then removed
+        // add the client as beneficiary
+        if(req.body.beneficiaries && req.body.beneficiaries.length) {
+            req.body.beneficiaries.map(async(b) => {
+                await db.insertDocument('beneficiaries', {
+                    clientId: clientId.insertedId,
+                    name: b.name,
+                    address: b.address,
+                    fiscalNumber: b.fiscalNumber,
+                    isClient: b.isClient,
+                    createdAt: new Date()
+                })
+            })
+        } else {
+            await db.insertDocument('beneficiaries', {
             clientId: clientId.insertedId,
             name: req.body.name,
             address: req.body.address,
@@ -48,26 +52,28 @@ router.post('/', async (req, res) => {
             isClient: true,
             createdAt: new Date()
         })
+        }
+        // If there is already a service to add
+        if(req.body.service) {
+            await db.insertDocument('services', {
+                clientId: clientId.insertedId,
+                type: req.body.service.type,
+                regime: req.body.service.regime,
+                shifts: req.body.service.shifts,
+                beginning: req.body.service.beginning,
+                duration: req.body.service.duration,
+                isActive: true,
+                createdAt: new Date()
+            })
+        }
+        res.status(200).send(clientId);
     }
-    // If there is already a service to add
-    if(req.body.service) {
-        await db.insertDocument('services', {
-            clientId: clientId.insertedId,
-            type: req.body.service.type,
-            regime: req.body.service.regime,
-            shifts: req.body.service.shifts,
-            beginning: req.body.service.beginning,
-            duration: req.body.service.duration,
-            isActive: true,
-            createdAt: new Date()
-        })
-    }
-    res.status(200).send(clientId);
 });
 
 // Update client
 router.put('/:id', async (req, res) => {
     await db.updateDocument('clients', {_id: new mongodb.ObjectId(req.params.id)}, req.body);
+    // If client is also beneficiary edit it as well
     await db.updateDocument('beneficiaries', {clientId: new mongodb.ObjectId(req.params.id), isClient: true}, req.body);
     res.status(200).send();
 });
