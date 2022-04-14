8e69085268650ec768589cb4eb0f8c30f41edd8d
Miguel Leitão <mleitao27@gmail.com>
2022-04-14 09:46:54 +0100

    messing with shifts' dates

--- cm-app-server/server/modules/shifts.js
+++ cm-app-server/server/modules/shifts.js
@@ -3,11 +3,39 @@ const db = require('./db');
 
 var generateShifts = async (clientId) => {
     let res = false;
+    let resData = {};
 
     let services = await db.getDocument('services', {clientId: new mongodb.ObjectId(clientId), isActive: true});
+    // Check if beginning this month or the next
+
+    let now = new Date();
+    let sameMonth = true;
+    
+    let i = 0;
+    // From string to milliseconds
+    let start = Date.parse(services[0].beginning);
+    let end = Date.parse(services[0].beginning);
+
+    resData.days = [];
+
+    while (sameMonth) {
+        end = end + services[0].shifts * 60 * 1000;
+
+        if(now.getMonth() !== new Date(end).getMonth()) sameMonth = false;
+
+        resData.days.push({
+            clientId: new mongodb.ObjectId(clientId),
+            // Date format
+            start: new Date(start),
+            end: new Date(end) 
+        });
+
+        i = i + 1;
+        start = end;
+    }
     
     if(services.length === 1) res = true;
-    return [res, res ? services[0] : null]
+    return [res, res ? {...services[0], ...resData} : null]
 };
 
 // Export functions
